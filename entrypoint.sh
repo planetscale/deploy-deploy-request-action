80cd94c6bb54b263edf8ce48cc4aa835ed4d0eb3
Brian Morrison II <brian@brianmorrison.me>
2023-03-16 11:57:12 -0400

    fix entrypoint again

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ command="pscale deploy-request deploy $1 $2 --org $3 -f json"
 cmdout=$(eval $command)
 
 if [ "true" == "$4" ];then
-  . /.pscale/cli-helper-scripts/wait_for_deploy_request_merged.sh
+  . /.pscale/cli-helper-scripts/wait-for-deploy-request-merged.sh
   wait_for_deploy_request_merged 9 "$1" "$2" "$3" 60
 fi
