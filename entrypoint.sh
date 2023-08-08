d2a9d7a17546efd28f2125024f18726f81437ce7
Brian Morrison II <brian@planetscale.com>
2023-08-08 09:58:37 -0400

    default to latest dr num

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/sh -l
 
-. /.pscale/cli-helper-scripts/wait-for-dr-validation.sh 
-wait_for_dr_validation 9 "$1" "$2" "$3" 60
+dbname=$1
+drnum=$2
+orgname=$3
+waitformerge=$4
 
-command="pscale deploy-request deploy $1 $2 --org $3 -f json"
+if [ -z "$drnum" ]; then
+  drnumout=$(pscale deploy-request list $dbname --org $orgname -f json)
+  drnum=$(jq -n "$drnumout" | jq -r '.[0].number')
+fi
+
+. /.pscale/cli-helper-scripts/wait-for-dr-validation.sh 
+wait_for_dr_validation 9 "$dbname" "$drnum" "$orgname" 60
+
+command="pscale deploy-request deploy $dbname $drnum --org $orgname -f json"
 
 cmdout=$(eval $command)
 
@@ -12,7 +22,7 @@ if [ $ret -ne 0 ]; then
   exit $ret
 fi
 
-if [ "true" == "$4" ];then
+if [ "true" == "$waitformerge" ];then
   . /.pscale/cli-helper-scripts/wait-for-deploy-request-merged.sh
-  wait_for_deploy_request_merged 9 "$1" "$2" "$3" 60
+  wait_for_deploy_request_merged 9 "$dbname" "$drnum" "$orgname" 60
 fi
